6c65629362b695ab3d5165cfd10c5cfbe9b3069d
Kagheni <Kaghenimbale@gmail.com>
2023-03-08 08:37:41 +0200

    Add the multiply and devide method

--- index2.test.js
+++ index2.test.js
@@ -12,4 +12,34 @@ describe('My calculator', () => {
             expect(calculator.add(2, 2)).toBe(4);
         });
     })
+
+    describe('Substract', () => {
+        test('This is the substraction of a and b', () => {
+            expect(calculator.substract(1, 4)).toBe(-3);
+        });
+
+        test('This is the addition of a and b', () => {
+            expect(calculator.substract(2, 2)).toBe(0);
+        });
+    })
+
+    describe('Division', () => {
+        test('This is the division of a and b', () => {
+            expect(calculator.devide(10, 5)).toBe(2);
+        });
+
+        test('This is the division of a and b', () => {
+            expect(calculator.devide(2, 2)).toBe(1);
+        });
+    })
+
+    describe('Multiplication', () => {
+        test('This is the addition of a and b', () => {
+            expect(calculator.multiply(1, 4)).toBe(4);
+        });
+
+        test('This is the addition of a and b', () => {
+            expect(calculator.multiply(2, 2)).toBe(4);
+        });
+    })
 })
